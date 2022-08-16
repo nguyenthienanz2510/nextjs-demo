00344f7c04406f01ebad0bf722eb913af71339ac
nguyenthienan <nguyenthienanz2510@gmail.com>
2022-08-16 23:30:56 +0700

    test getserversideprops

---
+++ components/Layouts/MainLayout.js
@@ -0,0 +1,11 @@
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <header>header</header>
+      <main>{children}</main>
+      <footer>footer</footer>
+    </>
+  );
+};
+
+export default MainLayout;
