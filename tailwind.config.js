336240d46efea872ecbb4755c1e90c7af6f837b1
nguyenthienanz <nguyenthienanz2510@gmail.com>
2022-08-25 11:26:49 +0700

    setup style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-primary': '#377DFF',
+        'color-green': '#38CB89',
+        'color-yellow': '#FFAB00',
+        'color-red': '#FF5630',
+        'color-white': '#FFFFFF',
+        'color-gray': '#4E5D78',
+        'color-gray-80': 'rgba(78, 93, 120, 0.8)',
+        'color-gray-60': 'rgba(78, 93, 120, 0.6)',
+        'color-gray-40': 'rgba(78, 93, 120, 0.4)',
+        'color-gray-20': 'rgba(78, 93, 120, 0.2)',
+        'color-black': "000000",
+        'color-dark-primary': "191C21",
+        'color-dark-secondary': "212833",
+      },
+    },
   },
   plugins: [],
 };
